d32460486628d3ce893de2daaef0604c482d97e6
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-07-11 22:05:24 +0530

    minor changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -105,11 +105,24 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
+    title: "Freelance Developer",
+    company_name: "Freelance",
+    // icon: shopify,
+    // iconBg: "#383E56",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React.js Developer (Internship)",
     company_name: "Magic Quill Media",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Oct 2022 - Dec 2022",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -120,27 +133,14 @@ const experiences = [
   {
     title: "Visual Computing Intern",
     company_name: "RV College of Engineering",
-    icon: tesla,
-    iconBg: "#E6DEDD",
+    // icon: tesla,
+    // iconBg: "#E6DEDD",
     date: "Jan 2021 - Feb 2022",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Freelance Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Learnt Image Processing using Python.",
+      "Learnt usage of various libraries like OpenCV",
+      // "Implementing responsive design and ensuring cross-browser compatibility.",
+      // "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
 ];
