be0ee098ebc6ce24e9c1f07f3d1e65c9c017cc1d
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-07-16 13:35:18 +0530

    .

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = () => {
     return (
-        <div className='flex flex-wrap justify-center mt-9'>
+        <div className='flex flex-wrap xl:mt-12 justify-center mt-10'>
             <div className="ml-3 mr-3 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-auto px-2 mb-4 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Programming</h5>
                 <p className="font-normal text-gray-700 dark:text-gray-400">
