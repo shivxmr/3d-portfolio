02f7e176c01fb8192b55e3a9da04ea7400c9caef
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-07-16 13:35:09 +0530

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Shivam</span>
+            Hi, I'm <span className='text-[#915EFF]'>Shivama</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop user interfaces <br className='sm:block hidden' />
