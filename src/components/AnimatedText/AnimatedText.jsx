ca38ef49113f2e6b627e13b399a6e2bec98c6825
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-07-18 17:45:58 +0530

    .

--- src/components/AnimatedText/AnimatedText.jsx
+++ src/components/AnimatedText/AnimatedText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./AnimatedText.css"; // Stylesheet
 
 const AnimatedText = ({ text }) => {
-  return <span className="animation-gradient text-7xl font-bold sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">{text}</span>;
+  return <span className="animation-gradient font-bold sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">{text}</span>;
 };
 
 export default AnimatedText;
