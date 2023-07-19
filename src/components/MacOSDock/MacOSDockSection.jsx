a31ce528042b1b87fd9618e0372625c772d08c84
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-07-20 01:27:55 +0530

    .

--- src/components/MacOSDock/MacOSDockSection.jsx
+++ src/components/MacOSDock/MacOSDockSection.jsx
@@ -7,12 +7,12 @@ function MacOSDockSection() {
   return (
     <Section color={'bg-macOSBg'} align={'items-end'}>
       <div className="flex flex-col w-full items-center justify-end">
-        <div className="text-4xl md:mb-9 lg:mb-32 xl:mb-32">
+        <div className="text-4xl md:mb-32 lg:mb-32 xl:mb-32">
           <AnimatedText text={'Social Profiles'} />
         </div>
 
         {/* For mobiles where this animation is not visible, please visit on a desktop or laptop for the best experience. */}
-        <div className="p-48 inline  sm:hidden">
+        <div className="p-48 inline sm:hidden">
           <h1 className="font-spaceMono text-xl w-60 text-center text-spotifyWhite">
             The below animation can be properly enjoyed on hovering with a mouse! This website is optimized for desktops and laptops. Please visit on a desktop or laptop for the best experience.
           </h1>
