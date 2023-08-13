7d3dd81d08e3fb26a42548baddd6db09756a762a
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-08-13 11:35:00 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@material-tailwind/react'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>,
 )
