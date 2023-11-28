2d08264180de26ee69be499f10da59978cec3a58
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-11-28 18:02:44 +0530

    change

--- src/constants/index.js
+++ src/constants/index.js
@@ -187,32 +187,32 @@ const testimonials = [
 ];
 
 const projects = [
-  {
-    name: "Animazing - Frontend Animation Showcase",
-    description:
-      "This project was created to showcase my animation and front-end skills. The colors, animations, and transitions are inspired by some of the most well used software, applications and websites.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      // {
-      //   name: "javascript",
-      //   color: "green-text-gradient",
-      // },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "framer-motion",
-        color: "orange-text-gradient",
-      },
-    ],
-    image: animazing,
-    source_code_link: "https://shivxmr-animazing.netlify.app/",
-    deployment_link: "https://shivxmr-animazing.netlify.app",
-  },
+  // {
+  //   name: "Animazing - Frontend Animation Showcase",
+  //   description:
+  //     "This project was created to showcase my animation and front-end skills. The colors, animations, and transitions are inspired by some of the most well used software, applications and websites.",
+  //   tags: [
+  //     {
+  //       name: "react",
+  //       color: "blue-text-gradient",
+  //     },
+  //     // {
+  //     //   name: "javascript",
+  //     //   color: "green-text-gradient",
+  //     // },
+  //     {
+  //       name: "tailwind",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "framer-motion",
+  //       color: "orange-text-gradient",
+  //     },
+  //   ],
+  //   image: animazing,
+  //   source_code_link: "https://shivxmr-animazing.netlify.app/",
+  //   deployment_link: "https://shivxmr-animazing.netlify.app",
+  // },
   {
     name: "SUMZ - Summarize Articles",
     description:
